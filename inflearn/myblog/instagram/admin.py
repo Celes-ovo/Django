0cfac172e809439d3c9a1dda9589741caf21a064
Celes-ovo <74448836+Celes-ovo@users.noreply.github.com>
2022-07-17 20:05:05 +0900

    update

--- inflearn/myblog/instagram/admin.py
+++ inflearn/myblog/instagram/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# 방법 3
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # 필드가 나오게 됨.
+    list_display = ['id', 'message', 'message_length', 'created_at', 'updated_at']
+
+    # 메세지에 링크를 만들어주는 역할
+    list_display_links = ['message']
+
+    # search_fields 속성
+	search_fields = ['message']
+
+    # list_filter 속성
+    list_filter = ['created_at']
+
+# 방법 1
+# admin.site.register(Post)
+
+# 방법 2
+# admin.site.register(Post, PostAdmin)
--- inflearn/myblog/instagram/models.py
+++ inflearn/myblog/instagram/models.py
@@ -3,5 +3,20 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     message = models.TextField() # default : blank=False
+    is_public = models.BooleanField(default=False, verbose_name='공개 여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+    # Java의 toString
+    def __str__(self):
+        #return f"Custom post object ({self.id})"
+        return self.message
+
+    # admin.py에 구현할 수도 있지만, 이번에는 model.py에 구현하기로 함.
+    def message_length(self):
+        #return len(self.message)
+        return f'{len(self.message)}글자'
+
+    # short_description
+    message_length.short_description = '메세지 글자 수'
