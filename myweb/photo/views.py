52eb49469e01cf5f6157d82215daa99b237ec615
celes <tkdals0617@gachon.ac.kr>
2024-06-25 15:47:54 +0900

    20240625 웹 페이지에 이미지를 post및 수정하는 과정

--- myweb/photo/views.py
+++ myweb/photo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Photo
+from .forms import PhotoForm
 
 # Create your views here.
 
@@ -12,3 +13,42 @@ def photo_detail(request, pk):
     # pk = Model의 데이터들을 구분하는 Django의 기본 ID 값
     photo = get_object_or_404(Photo, pk=pk)
     return render(request, 'photo/photo_detail.html', {'photo': photo})
+
+def photo_post(request, pk):
+    # 수정할 대상을 pk를 이용하여 찾아 옴.
+    photo = get_object_or_404(Photo, pk=pk)
+
+    # 들어온 요청이 POST일 경우
+    if request.method == "POST":
+        form = PhotoForm(request.POST)
+
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.save()
+
+            return redirect('photo_detail', pk = photo.pk)
+    
+    # 들어온 요청이 GET일 경우    
+    else:
+        # photo data를 PhotoForm에 담아서 넘겨 줌.
+        form = PhotoForm(instance=photo)
+    
+    return render(request, 'photo/photo_post.html', {'form':form})
+
+
+def photo_edit(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+
+    if request.method == "POST":
+        form = PhotoForm(request.POST, instance=photo)
+
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.save()
+
+            return redirect('photo_detail', pk=photo.pk)
+        
+    else:
+        form = PhotoForm(instance=photo)
+    
+    return render(request, 'photo/photo_post.html', {'form':form})
