8b4362fd2091c8057d42624f0fc18aff93971a46
Celes <tkdals0617@gachon.ac.kr>
2025-10-17 17:51:41 +0900

    25.10.17 : Update api part

--- gamesapi/gamesapi/settings.py
+++ gamesapi/gamesapi/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print("BASE_DIR:", BASE_DIR)
+# BASE_DIR = os.path.join(os.getcwd(), 'gamesapi')
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,6 +59,29 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'gamesapi.urls'
 
+WSGI_APPLICATION = 'gamesapi.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+## Rest Framework 설정
+# settings.py
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -71,19 +97,6 @@ TEMPLATES = [
         },
     },
 ]
-
-WSGI_APPLICATION = 'gamesapi.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
