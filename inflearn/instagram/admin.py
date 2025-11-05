9664a0247a4eface4b63012510c7df7dc73c73a6
Celes <tkdals0617@gachon.ac.kr>
2025-11-05 18:00:21 +0900

    25.11.06 : Update inflearn 학습 part

--- inflearn/instagram/admin.py
+++ inflearn/instagram/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Post
+
+#
+from django.utils.safestring import mark_safe
+
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'image', 'image_tag', 'is_public', 'created_at', 'updated_at']
+    # 
+    list_diaplay_links = ['message']
+    list_filter = ['is_public']
+
+    def image_tag(self, post):
+        if post.image:
+            return mark_safe(f'<img src="{post.photo.url}" style="width=75px" />')
+
+    def message_length(self, post):
+        return f'{len(post.message)}'
--- inflearn/instagram/models.py
+++ inflearn/instagram/models.py
@@ -3,5 +3,16 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     message = models.TextField()
+    image = models.ImageField(blank=True)
+    image_tag = models.ImageField(blank=True)
+    is_public = models.BooleanField(default=False, verbose_name='공개 여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # JAVA의 toString과 유사
+    def __str__(self):
+        return f'Post object ({self.id})'
+    
+    # Test 용도
+    def meassage_text(self):
+        return self.message
